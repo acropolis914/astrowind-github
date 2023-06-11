2642115eac898ca337e1ea2bfdb953e52ea7d6d5
acropolis914 <hed-phsuarnaba@smu.edu.ph>
2023-06-12 03:59:44 +0800

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,8 +19,8 @@ const whenExternalScripts = (items = []) =>
   SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: acropolis914.github.io,
-  base: astrowwind-github,
+  site: "acropolis914.github.io",
+  base: "astrowwind-github",
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
   output: 'static',
